5bd1773b914cdc5c0491cf2f3bf1f2153c29d78f
mitchellk107 <mitchellkelly107@gmail.com>
2025-07-15 20:32:48 -0600

    Added Multiple commands to the case code and updated README.

---
+++ README.md
@@ -0,0 +1,9 @@
+myman is a beginner friendly command line tool that explains basic Linux commands in plain language with examples. 
+
+It was inspired from the "man" command and it is designed to help new users quickly understand common commands like cat, ls, and cd. |
+
+To install, make sure myman.bash is executable with chmod +x myman.bash, then run it once. For example, type:  ./myman.bash cat while in the directory to automatically set up a persistent alias in your ~/.bashrc. After that, reload your shell with source ~/.bashrc, and you can use the myman command globally, like myman ls or myman grep. 
+
+The script is easy to customize too! You just open it and add new commands under the case "$1" in section. 
+
+This tool was made by a beginner to Linux so feel free to improve as needed.
---
+++ myman.bash
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# This sets up the myman alias dynamically
+SCRIPT_PATH="$(realpath "$0")"
+ALIAS_LINE="alias myman=\"bash $SCRIPT_PATH\""
+
+# This adds the  alias to ~/.bashrc if not already made
+if ! grep -Fxq "$ALIAS_LINE" ~/.bashrc; then
+  echo "$ALIAS_LINE" >> ~/.bashrc
+  echo "Alias [myman] added to ~/.bashrc"
+  echo "Run 'source ~/.bashrc' or open a new terminal to use it."
+
+  # This attempts to activate the myman alias
+  if [ -n "$BASH_VERSION" ]; then
+    source ~/.bashrc && echo "Activated alias!"
+  fi
+fi
+
+# Commands
+case "$1" in
+  cat)
+    echo -e "\`cat\`: Shows the content inside a file.\nExample:\n  cat myfile.txt"
+    ;;
+  ls)
+    echo -e "\`ls\`: Lists all files and folders where you are.\nExample:\n  ls -l"
+    ;;
+  cd)
+    echo -e "\`cd\`: Changes the folder you are working in.\nExample:\n  cd Documents"
+    ;;
+  mkdir)
+    echo -e "\`mkdir\`: Creates a new folder.\nExample:\n  mkdir newfolder"
+    ;;
+  rm)
+    echo -e "\`rm\`: Deletes a file.\nExample:\n  rm file.txt"
+    ;;
+  rmdir)
+    echo -e "\`rmdir\`: Deletes an empty folder.\nExample:\n  rmdir oldfolder"
+    ;;
+  touch)
+    echo -e "\`touch\`: Creates a new empty file.\nExample:\n  touch file.txt"
+    ;;
+  echo)
+    echo -e "\`echo\`: Prints text on the screen or writes it to a file.\nExample:\n  echo hello"
+    ;;
+  pwd)
+    echo -e "\`pwd\`: Shows the current folder path you are in.\nExample:\n  pwd"
+    ;;
+  whoami)
+    echo -e "\`whoami\`: Shows the name of the user you are logged in as.\nExample:\n  whoami"
+    ;;
+  clear)
+    echo -e "\`clear\`: Clears everything on the terminal screen.\nExample:\n  clear"
+    ;;
+  cp)
+    echo -e "\`cp\`: Copies a file or folder to a new place.\nExample:\n  cp file1.txt file2.txt"
+    ;;
+  mv)
+    echo -e "\`mv\`: Moves or renames a file or folder.\nExample:\n  mv file.txt folder/"
+    ;;
+  history)
+    echo -e "\`history\`: Shows a list of commands you typed before.\nExample:\n  history"
+    ;;
+  man)
+    echo -e "\`man\`: Shows detailed help for a command.\nExample:\n  man ls"
+    ;;
+  "")
+    echo "You didn't type a command. Try: myman ls"
+    ;;
+  *)
+    echo "Sorry! I don't have information for '$1'."
+    ;;
+esac
